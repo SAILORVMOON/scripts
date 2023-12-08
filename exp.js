8f3178e3fe3ba791da3d4258cf7fdbb50c95974b
SAILOR MOON <lesha.nevskiy.05@mail.ru>
2023-12-08 10:43:02 +0500

    pol
    pol

--- exp.js
+++ exp.js
@@ -1,6 +1,13 @@
-function setMyVar() {
-    var myVar = 2;
-  }
-  
-  setMyVar();
-  WSH.Echo(myVar);
+var F = new ActiveXObject("Scripting.FileSystemObject").OpenTextFile("Primer.txt");
+var a = F.ReadLine();
+var arr = F.ReadAll().split('\n');
+var nums = {}
+for(i = 0; i < arr.length; i++){
+    line = arr[i].split(" ");
+    nums[line[0]] = line[1];
+}
+WSH.Echo(a)
+WSH.Echo(nums["a"])
+WSH.Echo(nums["b"])
+WSH.Echo(nums["c"])
+F.close();
--- pol.js
+++ pol.js
@@ -13,10 +13,20 @@ function getPriority(oper) {
         return 2;
 }
 
+var F = new ActiveXObject("Scripting.FileSystemObject").OpenTextFile("Primer.txt");
+var primer = F.ReadLine();
+var arr = F.ReadAll().split('\n');
+var nums = {};
+for(i = 0; i < arr.length; i++){
+    line = arr[i].split(" ");
+    nums[line[0]] = line[1];
+}
+F.close();
+
 
 var amogus = ["+", "-", "*", "/", "^"];
-var primer = WSH.StdIn.ReadLine();
 var result = "";
+var resultm = [];
 var stack = [];
 for (var i = 0; i < primer.length; i++) {
     WSH.Echo(stack);
@@ -26,6 +36,7 @@ for (var i = 0; i < primer.length; i++) {
         var step = stack.pop();
         do {
             result += step;
+            resultm.push(step);
             step = stack.pop();
         } while (step != "(")
     } else if (inMas([primer.charAt(i)], amogus)) {
@@ -34,15 +45,65 @@ for (var i = 0; i < primer.length; i++) {
         else if (getPriority(primer.charAt(i)) > getPriority(stack[stack.length-1]))
             stack.push(primer.charAt(i));
         else {
-            while (getPriority(primer.charAt(i)) <= getPriority(stack[stack.length-1]))
-				result += stack.pop();
+            while (getPriority(primer.charAt(i)) <= getPriority(stack[stack.length-1])) {
+                var step = stack.pop();
+                result += step;
+                resultm.push(step);
+            }
+				
             stack.push(primer.charAt(i));
         }
-    } else
+    } else {
         result += primer.charAt(i);
+        resultm.push(primer.charAt(i));
+    }
+        
 }
-WSH.Echo(stack)
-while (stack.length > 0)
-	result += stack.pop();
- WSH.Echo(result);
+while (stack.length > 0){
+    var step = stack.pop();
+                result += step;
+                resultm.push(step);
+}
+
+WSH.Echo(result);
+WSH.Echo(resultm);
+
+stack = [];
+while(resultm.length != 0){
+    var step = resultm.shift();
+    while(!inMas(step, amogus)){
+        stack.push(nums[step]);
+        step = resultm.shift();
+    }
+    if(step == "+"){
+        var first = parseInt(stack.pop());
+        var second = parseInt(stack.pop());
+        var third = first + second;
+        stack.push(third);
+    } else if(step == "-"){
+        var first = parseInt(stack.pop());
+        var second = parseInt(stack.pop());
+        var third = first - second;
+        stack.push(third);
+    } else if(step == "*"){
+        var first = parseInt(stack.pop());
+        var second = parseInt(stack.pop());
+        var third = first * second;
+        stack.push(third);
+    } else if(step == "/"){
+        var first = parseInt(stack.pop());
+        var second = parseInt(stack.pop());
+        var third = first / second;
+        stack.push(third);
+    } else if(step == "^"){
+        var first = parseInt(stack.pop());
+        var second = parseInt(stack.pop());
+        var third = Math.pow(first, second);
+        stack.push(third);
+    } 
+}
+WSH.Echo(stack[0])
+
+
+
 
